854bf15be0561dde4fb4c97584e6932f23bae06f
omega <heyome9a@gmail.com>
2024-10-23 17:39:25 +0000

    service page seo done

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from "next/types";
 import Index from "@/components/Pages/Service/Index";
+
+export const metadata: Metadata = {
+  title: "Services - 24x7 Rudra Creative Home Decor & Architects",
+  description:
+    "Explore the professional services offered by 24x7 Rudra Creative Home Decor & Architects in the Patna region. We specialize in home decor, interior design, and architectural services to create beautiful, functional spaces.",
+  keywords: [
+    "services",
+    "home decor services",
+    "interior design",
+    "architectural services",
+    "Patna home decor",
+    "Patna interior design",
+    "Patna architecture",
+    "24x7 Rudra Creative Home Decor & Architects",
+    "Patna region",
+  ],
+};
 
 function page() {
   return (
